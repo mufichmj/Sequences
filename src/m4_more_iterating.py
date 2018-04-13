e72445a1485913cf4fd4c86b37464ef3f78c85c0
mufichmj <mufichmj@rose-hulman.edu>
2018-04-13 19:55:23 -0400

    m4

--- src/m4_more_iterating.py
+++ src/m4_more_iterating.py
@@ -538,7 +538,7 @@ def count_positive_sines(numbers):
 def run_test_sum_first_n():
     """ Tests the   sum_first_n   function. """
     # ------------------------------------------------------------------
-    # TODO: 8. Implement this TEST function.
+    # DONE: 8. Implement this TEST function.
     #   It TESTS the  sum_first_n  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests.
     #
@@ -608,6 +608,19 @@ def run_test_sum_first_n():
     print('       actual:  ', actual)
 
     # TO DO 8 (continued):  Add your 2 ADDITIONAL tests here:
+    # Test 8
+    expected = 10
+    actual = sum_first_n([1, 2, 3, 4, 5, 6], 4)
+    print()
+    print('Test 8 expected:', expected)
+    print('       actual:  ', actual)
+
+    # Test 9
+    expected = 15
+    actual = sum_first_n([1, 2, 3, 4, 5, 6], 5)
+    print()
+    print('Test 9 expected:', expected)
+    print('       actual:  ', actual)
 
 
 def sum_first_n(numbers, n):
