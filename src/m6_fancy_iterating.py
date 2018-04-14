efe68c8dfcc5d43854bfc2355d9122d6d8eb7b68
mufichmj <mufichmj@rose-hulman.edu>
2018-04-14 00:37:18 -0400

    m6

--- src/m6_fancy_iterating.py
+++ src/m6_fancy_iterating.py
@@ -512,12 +512,15 @@ def print_items_that_are_odd_integers(sequence):
       33 is at index 5
     """
     # ------------------------------------------------------------------
-    # TODO: 9. Implement and test this function.
+    # DONE: 9. Implement and test this function.
     #
     # IMPORTANT:  The  type  function returns  int  if its argument
     #   is an integer.  Note that   int   has NO quotes surrounding it.
     # ------------------------------------------------------------------
-
+    for k in range(len(sequence)):
+        if type(sequence[k]) is int:
+            if sequence[k] % 2 == 1:
+                print(sequence[k], 'is at index', k)
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # ----------------------------------------------------------------------
